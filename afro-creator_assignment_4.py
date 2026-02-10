95a4c7c0ad3aa608ec249dd2864a0d81181df970
afro-creator745 <hacole@aggies.ncat.edu>
2026-02-10 11:49:17 -0500

    adding the display greeting for required variables afro-creator_assignment_4.py

--- afro-creator_assignment_4.py
+++ afro-creator_assignment_4.py
@@ -1,5 +1,11 @@
 student_name = "Hakeem Cole"
 current_gpa = 3.9
 study_hours = 4
-social points = 45
+social_points = 45
 stress_level = 40
+print("Welcome", student_name + "!")
+print("Current GPA:", current_gpa)
+print("Student GPA:", current_gpa)
+print("Study hours:", study_hours)
+print("Social points:", social_points)
+print("Stress level:", stress_level)
